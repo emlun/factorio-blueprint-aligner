d1322b1488571b7d4e200feea1c5f8ea729bfc79
Emil Lundberg <emil@emlun.se>
2021-03-16 02:12:24 +0100

    Remove absolute alignment shortcut and input

--- src/data.lua
+++ src/data.lua
@@ -1,19 +1,5 @@
 local mod_defines = require("mod_defines")
 
-local align_absolute_input = {
-  type = "custom-input",
-  name = mod_defines.input.align_absolute,
-  key_sequence = "CONTROL + SHIFT + B",
-}
-
-local align_absolute_shortcut = {
-  type = "shortcut",
-  name = mod_defines.prototype.shortcut.align_absolute,
-  associated_control_input = align_absolute_input.name,
-  action = "lua",
-  icon = data.raw["shortcut"]["give-blueprint"]["icon"],
-}
-
 local align_relative_input = {
   type = "custom-input",
   name = mod_defines.input.align_relative,
@@ -54,7 +40,7 @@ local grid_selection_tool = {
 local set_grid_input = {
   type = "custom-input",
   name = mod_defines.input.set_grid,
-  key_sequence = "CONTROL + SHIFT + G",
+  key_sequence = "CONTROL + SHIFT + B",
 }
 
 local set_grid_shortcut = {
@@ -99,8 +85,6 @@ local move_grid_right_input = {
 
 
 data:extend{
-  align_absolute_shortcut,
-  align_absolute_input,
   align_relative_shortcut,
   align_relative_input,
   clear_cursor_input,
